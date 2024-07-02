ac0e682e029836b1a044001945b821f59f343c53
nabeman <manabe@iplab.cs.tsukuba.ac.jp>
2024-07-02 15:50:24 +0900

    ittann

--- index.js
+++ index.js
@@ -1,53 +1,79 @@
-document.addEventListener('mousemove', getMouse);
-let screenlog = document.querySelector('#screen-log')
+const canvas = document.getElementById("canvas");
 
+canvas.addEventListener('pointermove', getMouse);
+canvas.addEventListener('pointerdown', start);
+canvas.addEventListener('pointerup', stop);
+let screenlog = document.querySelector('#screen-log')
+const ctx = canvas.getContext("2d");
+const presenter = navigator.ink.requestPresenter({ presentationArea: canvas });
+let move_cnt = 0;
+
+//ペン先が触れているかどうかのフラグ
+let FLAG = false;
+let count = 1;
+const style = { color:"black", diameter: 10 }
 let x = 0;
 let y = 0;
 
-// getTimeStamp();
-
-function getMouse(e){
-    x = e.screenX;
-    y = e.screenY;
-
+setInterval(() => {
     screenlog.innerText = `
-        Screen X/Y: ${e.screenX}, ${e.screenY}
-    `
+            Screen X/Y: ${x}, ${y}
+        `
+    console.log(Date.now());
+}, 16.6666)
 
-    console.log("x:" + x + "  y:" + y );
-}
+async function getMouse(e){
+    if(FLAG){
+        x = e.screenX;
+        y = e.screenY;
 
-const cameraWidth = 300;
-const cameraHeight = 400;
+        const pointSize = 5;
+        ctx.fillStyle = "black";
+        ctx.fillRect(x, y, pointSize, pointSize);
+    
+        await presenter.updateInkTrailStartPoint(e, style);
 
-const cameraInit = () => {
-    const video = document.getElementById("camera");
-
-    const cameraSetting = {
-        audio: false,
-        video: {
-            width: cameraWidth,
-            height: cameraHeight,
-            facingMode: "environment",
-        }
+        console.log(count);
+        count++;
     }
-
-    navigator.mediaDevices.getUserMedia(cameraSetting)
-        .then((mediaStream) => {
-            video.srcObject = mediaStream;
-        })
-        .catch((err) => {
-            console.log(err.toString());
-        });
 }
-// setInterval(() => {
-//     console.log("x:" + x + "  y:" + y );
-// }, 500);
 
-// function getTimeStamp(){
-//     let time = Date.now();
+function start(){
+    FLAG = true;
+}
 
-//     console.log("x:" + x + "  y:" + y + "  time:" + time);
+function stop(){
+    FLAG = false;
+}
 
-//     requestAnimationFrame(getTimeStamp);
+// async function draw(){
+//     const pointSize = 5;
+//     ctx.fillStyle = "black";
+//     ctx.fillRect(x, y, pointSize, pointSize);
+
+//     await presenter.updateInkTrailStartPoint(evt, style);
+// }
+
+// const cameraWidth = 300;
+// const cameraHeight = 400;
+
+// const cameraInit = () => {
+//     const video = document.getElementById("camera");
+
+//     const cameraSetting = {
+//         audio: false,
+//         video: {
+//             width: cameraWidth,
+//             height: cameraHeight,
+//             facingMode: "environment",
+//         }
+//     }
+
+//     navigator.mediaDevices.getUserMedia(cameraSetting)
+//         .then((mediaStream) => {
+//             video.srcObject = mediaStream;
+//         })
+//         .catch((err) => {
+//             console.log(err.toString());
+//         });
 // }
