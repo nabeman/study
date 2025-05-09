ff14543eae0350fce06cdec7731f173abb45a94d
nabeman <manabe@iplab.cs.tsukuba.ac.jp>
2025-05-09 17:33:49 +0900

    色々な部分の実行時間を測った

--- Depth-Anything-V2/depth_infer.py
+++ Depth-Anything-V2/depth_infer.py
@@ -21,15 +21,26 @@ model_configs = {
     'vitg': {'encoder': 'vitg', 'features': 384, 'out_channels': [1536, 1536, 1536, 1536]}
 }
 
+start1 = time.perf_counter()
 model = DepthAnythingV2(**{**model_configs[encoder]})
+middle3 = time.perf_counter()
 model.load_state_dict(torch.load(f"depth_anything_v2_{encoder}.pth", map_location = "cpu"))
+middle4 = time.perf_counter()
 model = model.to(DEVICE).eval()
 # to(DEVICE): デバイス(GPU/CPU)を切り替える
 # eval: 評価モードに切り替え
+middle1 = time.perf_counter()
+print("ライブラリの読み込み: {}".format(middle3 - start1))
+print("モデルのロード: {}".format(middle4 - middle3))
+print("デバイスの切り替え: {}".format(middle1 - middle4))
+print("middle1までの実行時間: {}".format(middle1 - start1))
 
 # raw_img = cv2.imread("C:\\Users\\tkmco\\study\\Depth-Anything-V2\\images\\image1.jpg")
-raw_img = cv2.imread("./images/testimages/nottouch_near.jpg")
+raw_img = cv2.imread("./images/testimages/maue_25.jpg")
 # with torch.no_grad():
+middle2 = time.perf_counter()
+print("middle2までの実行時間: {}".format(middle2 - middle1))
+
 start = time.perf_counter()
 depth = model.infer_image(raw_img)
 end = time.perf_counter()
@@ -42,16 +53,16 @@ depth_color = (cmap(depth_color)[:, :, :3] * 255)[:, :, ::-1].astype(np.uint8)
 
 depth_color = cv2.resize(depth_color, None, fx=0.25, fy=0.25)
 
-if len(depth_color.shape) == 3:
-# 各ピクセルがグレースケールかどうかを確認
-    if np.all(depth_color[:,:,0] == depth_color[:,:,1]) and np.all(depth_color[:,:,1] == depth_color[:,:,2]):
-        print('この画像はグレースケールです。')
-    else:
-        print('この画像はカラー画像です。')
-else:
-    print('この画像は既にグレースケールです。')
+# if len(depth_color.shape) == 3:
+# # 各ピクセルがグレースケールかどうかを確認
+#     if np.all(depth_color[:,:,0] == depth_color[:,:,1]) and np.all(depth_color[:,:,1] == depth_color[:,:,2]):
+#         print('この画像はグレースケールです。')
+#     else:
+#         print('この画像はカラー画像です。')
+# else:
+#     print('この画像は既にグレースケールです。')
 
-print(depth_color.shape)
+# print(depth_color.shape)
 depth = cv2.resize(depth, None, fx=0.25, fy=0.25)
 def onMouse(event, x, y, flags, params):
     if event == cv2.EVENT_LBUTTONDOWN:
@@ -60,10 +71,12 @@ def onMouse(event, x, y, flags, params):
         print(depth[y, x])
 
 # Record(317, 269, depth)
+end1 = time.perf_counter()
+print("最後の部分の実行時間: {}".format(end1 - end))
+print("全体の実行時間: {}".format(end1 - start1))
 
 cv2.imshow('depth', depth_color)
 cv2.setMouseCallback('depth', onMouse)
-print(cv2.getWindowImageRect('depth'))
 cv2.waitKey(0)
 cv2.destroyAllWindows()
     # cv2.imwrite('result.jpg', depth)
