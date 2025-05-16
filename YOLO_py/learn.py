cff68480f6b681f256477fda1533fce283cfab95
nabeman <manabe@iplab.cs.tsukuba.ac.jp>
2025-05-16 16:38:39 +0900

    a

--- YOLO_py/learn.py
+++ YOLO_py/learn.py
@@ -1,5 +1,5 @@
 from ultralytics import YOLO
 
 model = YOLO("yolov8n.pt")
-model.train(data="./original_set2/data.yaml", epochs=300)
+model.train(data="./original_set4/data.yaml", epochs=300)
 metrics = model.val()
