c92c8fa1026481d7adbb2ff5c3bc447ecd54a33a
nabeman <manabe@iplab.cs.tsukuba.ac.jp>
2025-05-23 15:38:30 +0900

    描画処理を変更

--- Depth-Anything-V2/app/main.py
+++ Depth-Anything-V2/app/main.py
@@ -34,6 +34,7 @@ cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 def main():
     yolo_model = YOLO("../runs/detect/train7/weights/best.pt")
     tip_point = []
+    touch_flag = False
     # mask = None
     while True:
         ret, img = cap.read()
@@ -47,18 +48,38 @@ def main():
             for box, class_id in zip(r.boxes.xyxy, r.boxes.cls):
                 class_name = r.names[int(class_id)]
                 if class_name == "tip":
+                    # ペン先が検出された場合、ボックスの左上と右下の座標を取得
                     x1 = box[0].item()
                     y1 = box[1].item()
                     x2 = box[2].item()
                     y2 = box[3].item()
         
-        if not x1 == None:
+        if not x1 == None: #ペン先が検出された場合ペン先周りの深度のばらつき(標準偏差)を計算
             std = np.std(depth[(int(y1)-30):(int(y2)+30), (int(x1)-30):(int(x2)+30)])
-            if std < 0.4:
+            if std < 0.4: # ペン先が接触していた場合 (深度のばらつきが閾値以下)
                 tip_point.append([int(x2), int(y2)])
-        if len(tip_point) > 0:
-            for i in range(len(tip_point)-1):
-                cv2.line(img, (tip_point[i][0], tip_point[i][1]), (tip_point[i+1][0], tip_point[i+1][1]), (255, 0, 0), thickness=1)    
+                touch_flag = True
+            else:
+                touch_flag = False   
+
+        # ペン先の座標をストロークごとに保存する処理
+        if touch_flag:
+            if len(tip_point) != 0:
+                tip_point[len(tip_point)-1].append([int(x2), int(y2)])
+            else:
+                tip_point.append([])
+                tip_point[0].append([int(x2), int(y2)])
+        else:
+            if len(tip_point) != 0:
+                tip_point.append([])
+
+        # ストロークの描画
+        for i in tip_point:
+            if len(i) > 1: #接触点が2つ以上保存された場合
+                for j in range(len(i)-1):
+                    cv2.line(img, (i[j][0], i[j][1]), (i[j+1][0], i[j+1][1]), (255, 0, 0), thickness=1)
+            elif len(i) == 1:
+                cv2.circle(img, (i[j][0], i[j][1]), 2, (255, 0, 0), thickness=1)
 
         cv2.imshow("Video", img)
         if cv2.waitKey(1) & 0xFF == ord('q'):
